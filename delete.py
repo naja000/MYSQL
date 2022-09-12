20f4211d672fae70d227eb2457076a70822c2415
naja000 <110969361+naja000@users.noreply.github.com>
2022-09-12 15:38:14 +0530

    Add files via upload

---
+++ delete.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+conn=mysql.connector.connect(
+    user='root',password='abc123',host='127.0.0.1',database='mysql')
+cursor=conn.cursor()
+print("Content of the table:")
+cursor.execute("SELECT * from EMPLOYEE")
+print(cursor.fetchall())
+sql="DELETE FROM EMPLOYEE WHERE AGE>'%d'" % (25)
+
+
+try:
+    cursor.execute(sql)
+    conn.commit()
+except:
+    conn.rollback()
+print("Contents of the table after delete operation")
+cursor.execute("SELECT *from EMPLOYEE")
+print(cursor.fetchall())
+conn.close()
