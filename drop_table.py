bf06a3ab549863a081c2b22ae91772419ac434ef
naja000 <110969361+naja000@users.noreply.github.com>
2022-09-12 15:38:36 +0530

    Add files via upload

---
+++ drop_table.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+conn=mysql.connector.connect(
+    user='root',password='abc123',host='127.0.0.1',database='mysql')
+cursor=conn.cursor()
+cursor.execute("SHOW Tables")
+print(cursor.fetchall())
+
+cursor.execute("DROP TABLE EMPLOYEE")
+print("Table droped... ")
+
+print("List of table after dropping the EMPLOYEE table")
+cursor.execute("SHOW tables")
+print(cursor.fetchall())
+conn.close()
